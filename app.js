4554841cde8c9284c2bcfa461d5ddcc925c5ce8e
Hyerim Han <hyerim_han@naver.com>
2022-10-27 22:56:34 +0900

    two WebApp

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+const container = document.getElementById('root')
 const ajax = new XMLHttpRequest();
-
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
-ajax.open('GET', NEWS_URL, false);
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
+ajax.open('GET', NEWS_URL, false);
 // 데이터 가져오기
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function() {
+  const id = location.hash.substr(1);
+
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+
+  title.innerHTML = newsContent.title
+  content.appendChild(title)
+})
+
 for(let i = 0; i < newsFeed.length; i += 1) {
   const li = document.createElement('li');
+  const a = document.createElement('a');
 
-  li.innerHTML = newsFeed[i].title
-  ul.appendChild(li)
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
+  ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
