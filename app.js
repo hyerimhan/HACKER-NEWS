98ca031dcf0fb0ca6eb41afacc71a39bea1ba033
Hyerim Han <hyerim_han@naver.com>
2022-10-28 11:39:34 +0900

    feat: router

--- app.js
+++ app.js
@@ -12,33 +12,50 @@ function getData(url) {
   return JSON.parse(ajax.response)
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener('hashchange', function() {
+  newsList.push(`<ul>`);
+
+  for(let i = 0; i < newsFeed.length; i += 1) {
+    newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `);
+  }
+
+  newsList.push(`</ul>`);
+
+  container.innerHTML = newsList.join('')
+}
+
+function newsDetail() {
   const id = location.hash.substr(1);
 
   const newsContent = getData(CONTENT_URL.replace('@id', id));
   const title = document.createElement('h1');
 
-  title.innerHTML = newsContent.title
-  content.appendChild(title)
-})
-
-for(let i = 0; i < newsFeed.length; i += 1) {
-  const div = document.createElement('div')
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-      ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>`
-
-  ul.appendChild(div.children[0]);
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    
+    <div>
+      <a href="#">목록으로</a>
+    </div>`;
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+  const routePath = location.hash;
+
+  if(routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+window.addEventListener('hashchange', router)
+
+router();
